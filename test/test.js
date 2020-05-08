04d70001c9b05a562ad03a5b4467edc5de56056c
dexfire <dexfire29@foxmail.com>
2020-05-08 21:37:51 +0800

    update & renamed the testcase file

--- test/test.js
+++ test/test.js
@@ -4,5 +4,5 @@ const m = require('..')
 
 describe('markdown-it-todo', () => {
   const md = markdownIt().use(m)
-  generate(`${__dirname}/fixtures/enml-todo.txt`, {header: true}, md)
+  generate(`${__dirname}/fixtures/markdown-it-todo.txt`, {header: true}, md)
 })
